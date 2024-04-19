6fe98488fc55a05f621f729e89fd6202e9f7afec
g202235440 <tomport@naver.com>
2024-04-19 15:48:55 +0900

    .
    .

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from typing import Union
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    URL= "https://bigdata.kepco.co.kr/cmsmain.do?scode=S01&pcode=000167&pstate=L&redirect=Y"
+
+    contents = requests.get(URL).text
+
+    return {"message": contents}
 
 @app.get("/item/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
