27daae3c4a413e8576be91a1a1f5b149b47989df
g202235440 <126758198+g202235440@users.noreply.github.com>
2024-05-08 17:48:03 +0900

    .
    .

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
-from typing import Union
-import requests
-
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    URL= "https://bigdata.kepco.co.kr/cmsmain.do?scode=S01&pcode=000167&pstate=L&redirect=Y"
-
-    contents = requests.get(URL).text
-
-    return {"message": contents}
+def root():
+    return {"message": "hello world"}
