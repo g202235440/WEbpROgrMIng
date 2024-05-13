60585cef1db3e482d97d333ad45b3112f2f85e6e
g202235440 <126758198+g202235440@users.noreply.github.com>
2024-05-14 02:44:24 +0900

    해냈다
    해냈어

--- api/infile/test1.py
+++ api/infile/test1.py
@@ -1,9 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
+import api.cruds.task as task_crud
+from api.db import get_db
+import api.schemas.task as task_schema
+
 @router.get("/")
-def create():
+def hello():
     return{"message":"hello"}
     
-    
+
+@router.post("/tasks",response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body:task_schema.TaskCreate,db:Session = Depends(get_db)):
+    return task_crud.create_task(db,task_body)
+
+@router.get("/tasks",response_model=list[task_schema.Task])
+async def list_task(db:Session = Depends(get_db)):
+    return task_crud.get_tasks_with_done(db)
--- api/models/table.py
+++ api/models/table.py
@@ -1,10 +1,21 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 from api.db import Base
+import requests
 
 class Task(Base):
     __tablename__="stock"
 
     id=Column(Integer, primary_key=True)
     title = Column(String(1024))
+
+    done = relationship("Done",back_populates="stock",cascade="delete")
+
+class Done(Base):
+    __tablename__="Done"
+
+    id=Column(Integer,ForeignKey("stock.id"),primary_key=True)
+    
+    stock = relationship("Task",back_populates="done")
