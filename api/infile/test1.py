7823b8a642099b5038d9ce06e08f2b29a2e84b94
g202235440 <126758198+g202235440@users.noreply.github.com>
2024-05-15 16:24:30 +0900

    api에서 가져와서 올리기 만듬
    이제 지우는거 필요할 듯
    근데 그거 안만들어도 상관은 없고
    일단 필요없는 done 지우기랑 필요한 데이터 베이스에 넣기랑 방복문으로 데이터 베이스에 저장하기 하면 될듯

--- api/infile/test1.py
+++ api/infile/test1.py
@@ -6,15 +6,23 @@ router = APIRouter()
 import api.cruds.task as task_crud
 from api.db import get_db
 import api.schemas.task as task_schema
+from api.api.request import get_data
 
 @router.get("/")
 def hello():
     return{"message":"hello"}
     
 
+#@router.post("/tasks",response_model=task_schema.TaskCreateResponse)
+#async def create_task(task_body:task_schema.TaskCreate,db:Session = Depends(get_db)):
+#    return task_crud.create_task(db,task_body)
+
 @router.post("/tasks",response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body:task_schema.TaskCreate,db:Session = Depends(get_db)):
+async def create_task(db:Session = Depends(get_db)):
+    data =get_data(1,1,'crno')
+    task_body = task_schema.TaskCreate(title=data)
     return task_crud.create_task(db,task_body)
+
 
 @router.get("/tasks",response_model=list[task_schema.Task])
 async def list_task(db:Session = Depends(get_db)):
