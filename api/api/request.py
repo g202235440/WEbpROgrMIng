63ab76781a0b147eb218744843b1f047abe62409
g202235440 <126758198+g202235440@users.noreply.github.com>
2024-05-23 01:58:11 +0900

    params->url
    params 쓰래기같은 코드 이유를 모르겠네 내 중요한 잠시간 3시간을 뺏은 너 저주한다

--- api/api/request.py
+++ api/api/request.py
@@ -1,16 +1,11 @@
 import requests
 import json
-def get_data(page_number,int,str):
+def get_data(page_number:int,num:int,star: str):
     base_url = "http://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo"
-    params = {
-        "pageNo": page_number,
-        "numOfRows": int,
-        "resultType": "json",
-        "serviceKey": "XjAOulAs6VTDHZEFyeermCWZGMt05tyiPeVEpWTocYYwFyMCMEKDr4ktytwl3n3HvLasZJWkO%2FN4JA1pCBGb0A%3D%3D"
-    }
-    response = requests.get(base_url, params=params)
+    url = f"{base_url}?pageNo={page_number}&numOfRows={num}&resultType=json&serviceKey=IqW18UXSortGuOqMaA2DhCn6opMxu2LSem7lh%2FaP5got1%2BfVgaRfLCQ9JK%2FrSg6gOTTnq8E6P6VomidLXkIEqA%3D%3D"
+    response = requests.get(url)
     contents = response.text
     json_ob = json.loads(contents)
     body=json_ob['response']['body']['items']['item']
-    values =''.join(item[str] for item in body)
+    values =''.join(item[star] for item in body)
     return values
--- api/infile/test1.py
+++ api/infile/test1.py
@@ -15,13 +15,13 @@ def hello():
 @router.post("/tasks", response_model=List[task_schema.TaskCreateResponse])
 async def create_task(db: Session = Depends(get_db)):
     created_tasks = []
-    for p in range(1, 10):
-        data = get_data(p, 1, 'itmsNm')
+    for p in range(1,3):
+        data = get_data(p,1,'itmsNm')
         task_body = task_schema.TaskCreate(title=data)
         created_task = task_crud.create_task(db, task_body)
         created_tasks.append(created_task)
     return created_tasks
 
-@router.get("/tasks",response_model=list[task_schema.Task])#TaskCreateResponse로 바꾸기
+@router.get("/tasks",response_model=list[task_schema.Task])
 async def list_task(db:Session = Depends(get_db)):
     return task_crud.get_tasks_with_done(db)
